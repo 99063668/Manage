fce2f0ad53c5c646ce6d3adf362e301a828e369f
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-06-01 10:03:49 +0200

    Add files via upload

---
+++ view/templates/footer.php
@@ -0,0 +1,4 @@
+<footer>&copy; Dewi van der Velden</footer>
+</div><!-- end container div -->
+</body>
+</html>
---
+++ view/templates/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Guest</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 	
+	<link rel="stylesheet" href="<?= URL ?>/css/style.css">
+</head>
+<body>
+<div id="container">
+<nav>
+	<ul>
+		<li><a href="<?=URL?>home/index">Home</a></li>
+		<li><a href="<?=URL?>guest/index">Guest</a></li>
+	</ul>
+</nav>

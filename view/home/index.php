934dab39c033aa215310d0e643ebb142d46bc024
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-06-08 08:34:49 +0200

    Add files via upload

--- view/home/index.php
+++ view/home/index.php
@@ -2,3 +2,7 @@
  <h1>Geplande reserveringen</h1>
  <p>reservering 1</p>
 </div>
+
+<a href="/guest/update/1">Wijzigen</a> <br>
+<a href="/guest/delete/1">Verwijderen</a> <br>
+<a href="/guest/add/1">Toevoegen</a>

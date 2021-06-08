b9be243f7698bde924afc46521b1d0a222d5b098
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-06-08 08:34:34 +0200

    Add files via upload

--- view/guest/index.php
+++ view/guest/index.php
@@ -1,27 +1,31 @@
 <?php
-	// maak een overzicht lijst van ALLE personen
 	$guests = getAllGuest();
 ?>
 <h1>Een reservering plannen</h1>
-<?php
-	foreach ($guests as $guest) {
-?>
-	<!-- <ul>
-		<li>
-			<span><?=$guest["name"]?> is <?=$guest["age"]?> jaar</span>
-		</li>
-	</ul> -->
-
-<?php	
-	}
-?>
-<a href="/guest/update/1">Wijzigen</a> <br>
-<a href="/guest/delete/1">Verwijderen</a> <br>
-<a href="/guest/add/1">Toevoegen</a>
-
-<?php
-// de opbouw van de link bepaald welke method in welke controller aangeroepen wordt.
-// het woordje "employee" in de url betekent dat het framework moet zoeken naar een controller genaamd "EmployeeController".
-// Hij maakt van de eerste letter een hoofdletter en plakt er zelf "Controller" achter.
-// Het woordje "update" of "delete" betekent dat hij in deze controller moet zoeken naar een method met deze naam.
-?>
+<a href="/guest/add/1">Reserveren</a>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Bezoeker</th>
+            <th scope="col">Begintijd</th>
+            <th scope="col">Paard</th>
+			<th scope="col">Prijs</th>
+            <th scope="col">Beheer reservering</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            foreach((array) $guests as $guest){
+        ?>
+        <tr>
+            <td><?=$guest["guest_name"]?></td>
+            <td><?=$guest["times"]?></td>
+            <td><?=$guest["horse"]?></td>
+			<td>€<?=$guest["price"]?></td>
+			<td><a href="/guest/update/1">Wijzigen</a> / <a href="/guest/delete/1">Verwijderen</a></td>
+        </tr>
+        <?php
+            }
+        ?>
+    </tbody>
+</table>

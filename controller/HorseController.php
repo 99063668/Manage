b519495b0e0790f155f70182d09e24ea8fd46567
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-06-08 11:04:41 +0200

    Add files via upload

--- controller/HorseController.php
+++ controller/HorseController.php
@@ -18,12 +18,45 @@
         render("horse/add");
     }
 
-    //Laad de add page in
+    //Laad de add function in
     function addHorses() {
         if($_SERVER["REQUEST_METHOD"] == "POST"){
             $data = controle();
             addHorse($data);
             index();
+        }
+    }
+
+    //Laad de add page voor pony's in
+    function openForm2() {
+        render("horse/addPony");
+    }
+
+    //Laad de add function voor pony's in
+    function addPonys() {
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $data = controle2();
+            addPony($data);
+            index();
+        }
+    }
+
+    //laad de delete page
+    function openDelete() {
+        render("horse/delete");
+    }
+
+    //ladd de delete function in
+    function deleteHorses() {
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            if (!empty($_POST["Delete"])) {
+                //Show confirm form
+            } elseif (!empty($_POST["Delete2"])) {
+                deleteHorse($_GET["id"]);
+                index();
+            } elseif (!empty($_POST["Delete3"])) {
+                index();
+            }
         }
     }
     

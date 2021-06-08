ec46d05af872d468326a632f6e696dd27a57923b
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-06-08 08:34:16 +0200

    Add files via upload

--- model/GuestModel.php
+++ model/GuestModel.php
@@ -1,35 +1,16 @@
 <?php
 	function getAllGuest(){
-		// Met het try statement kunnen we code proberen uit te voeren. Wanneer deze
-		// mislukt kunnen we de foutmelding afvangen en eventueel de gebruiker een
-		// nette foutmelding laten zien. In het catch statement wordt de fout afgevangen
 		try {
-			// Open de verbinding met de database
 			$conn=openDatabaseConnection();
-		
-			// Zet de query klaar door middel van de prepare method
 			$stmt = $conn->prepare("SELECT * FROM reservering");
-	
-			// Voer de query uit
 			$stmt->execute();
-	
-			// Haal alle resultaten op en maak deze op in een array
-			// In dit geval is het mogelijk dat we meedere medewerkers ophalen, daarom gebruiken we
-			// hier de fetchAll functie.
 			$result = $stmt->fetchAll();
 	
 		}
-		// Vang de foutmelding af
 		catch(PDOException $e){
-			// Zet de foutmelding op het scherm
 			echo "Connection failed: " . $e->getMessage();
 		}
-	
-		// Maak de database verbinding leeg. Dit zorgt ervoor dat het geheugen
-		// van de server opgeschoond blijft
 		$conn = null;
-	
-		// Geef het resultaat terug aan de controller
 		return $result;
 	}
 ?>
--- model/HorseModel.php
+++ model/HorseModel.php
@@ -1,4 +1,20 @@
 <?php
+	//haalt 1 paard/pony op uit de database
+	function getTable($table, $id){
+		$conn = openDatabase();
+
+		$id = intval($id);
+		if (($table == "horse" || $table == "pony") && isset($id) && !empty($id) && is_numeric($id)) {
+			$query = $conn->prepare("SELECT * FROM `$table` WHERE id = :id");
+			$query->bindParam(":id", $id);
+			$query->execute();
+			$result = $query->fetch(PDO::FETCH_ASSOC);
+
+			return $result;
+		}
+	}
+
+	//Haalt alle paarden op uit de database
 	function getAllHorse(){
 		try {
 			$conn=openDatabaseConnection();
@@ -14,10 +30,12 @@
 		return $result;
 	}
 
+	//Haalt alle pony's uit de database op
 	function getAllPony(){
 		try {
 			$conn=openDatabaseConnection();
 			$stmt = $conn->prepare("SELECT * FROM pony");
+			$stmt->execute();
 			$result = $stmt->fetchAll();
 	
 		}
@@ -27,5 +45,95 @@
 	
 		$conn = null;
 		return $result;
+	}
+
+	//Voegt een paard toe aan de database
+	function addHorse($data){
+        $conn = openDatabaseConnection();
+
+        if(!empty($data) && isset($data)){
+            if (isset($data["horse_name"]) && !empty($data["horse_name"]) && isset($data["horse_breed"]) && !empty($data["horse_breed"]) && isset($data["horse_age"]) && !empty($data["horse_age"]) && isset($data["jump"]) && !empty($data["jump"])) {
+                $stmt = $conn->prepare("INSERT INTO plannings(horse_name, horse_breed, horse_age, jump, players) VALUES (:horse_name, :horse_breed, :horse_age, :jump)");
+                $stmt->bindParam(":horse_name", $data["horse_name"]);
+                $stmt->bindParam(":horse_breed", $data["horse_breed"]);
+                $stmt->bindParam(":horse_age", $data["horse_age"]);
+                $stmt->bindParam(":jump", $data["jump"]);
+                $stmt->execute();
+
+                return $data;
+            }
+        }else{
+            echo("error empty post game bij function controle.");
+        }
+    }
+
+       // Verwijderd 1 paard uit de database
+       function deleteHorse($id){
+        $conn = openDatabaseConnection();
+        $id = intval($id);
+        $check = getTable("horse", $id);
+
+        if (!empty($id) && isset($id) && is_numeric($id) && !empty($check) && isset($check)){
+            $stmt = $conn->prepare("DELETE FROM horse WHERE id = :id");
+            $stmt->bindParam(":id", $id);
+            $stmt->execute(); 
+        } 
+    }
+
+	//Controlleert de input
+	function controle(){
+        $data = [];
+
+        if(!empty($_POST["horse_name"])){
+            $horse_name = trimdata($_POST["horse_name"]);
+            if(!preg_match("/^[a-zA-Z-' ]*$/", $horse_name)){
+                echo("Alleen letters en spaties zijn toegestaan!");
+            }else{
+                $data["horse_name"] = $horse_name;
+            }
+        }
+
+        if(!empty($_POST["horse_breed"])){
+            $horse_breed = trimdata($_POST["horse_breed"]);
+            if(!preg_match("/^[a-zA-Z-' , ]*$/", $horse_breed)){
+                echo("Alleen letters en spaties zijn toegestaan!");
+            }else{
+                $data["horse_breed"] = $horse_breed;
+            }
+        }
+		
+		if(!empty($_POST["horse_age"])){
+            $horse_age = trimdata($_POST["horse_age"]);
+            if(!preg_match("/[0-9]/", $horse_age)){
+                echo("Alleen cijfers zijn toegestaan!");
+            }else{
+                $data["horse_age"] = $horse_age;
+            }
+        }
+
+		if(!empty($_POST["jump"])){
+            $jump = trimdata($_POST["jump"]);
+            if(!preg_match("/^[a-zA-Z-' , ]*$/", $jump)){
+                echo("Alleen letters en spaties zijn toegestaan!");
+            }else{
+                $data["jump"] = $jump;
+            }
+        }
+        return $data;
+    }
+
+	//..
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if (!empty($_POST["SubmitBtn"])) {
+            $input = controle();
+            addHorse($input);
+		} elseif (!empty($_POST["Delete"])) {
+            //Show confirm form
+        } elseif (!empty($_POST["Delete2"])) {
+            deleteGame($_GET["id"]);
+            header("location: index.php");
+        } elseif (!empty($_POST["Delete3"])) {
+            header("location: index.php");
+        }
 	}
 ?>

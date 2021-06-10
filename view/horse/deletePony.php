462ab15c15f360f26f11dee4951c5ef937bf7427
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-06-10 10:08:37 +0200

    Add files via upload
    Verwijderen van paarden toegevoegd

--- view/horse/deletePony.php
+++ view/horse/deletePony.php
@@ -1,16 +1,16 @@
 <?php
     if(isset($_GET["id"])){
-        $horse= getTable("horse", $_GET["id"]);
-        if(!$horse){
-            echo "Paard bestaat niet";
+        $pony= getTable("pony", $_GET["id"]);
+        if(!$pony){
+            echo "Pony bestaat niet";
         }
     }else{
         echo "Verwijderen mislukt";
     }
 ?>
 
-<form action="<?=URL?>horse/deleteHorses?id=<?= $horse["id"]?>" method="post">
-    <label for="Confirm"><b>Are you sure you want to delete <?= $horse["horse_name"]?>?</b></label>
+<form action="<?=URL?>horse/deletePonys?id=<?= $pony["id"]?>" method="post">
+    <label for="Confirm"><b>Are you sure you want to delete <?= $pony["pony_name"]?>?</b></label>
     <button class="btn btn-success btn-sm" type="submit" name="Delete2" value="true">Yes</button>
     <button class="btn btn-danger btn-sm" type="submit" name="Delete3" value="false">Cancel</button>
 </form>
